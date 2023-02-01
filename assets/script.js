eab3061b041562615e486b63ea0f54187b2346f6
Seb <jonesseb10@gmail.com>
2023-02-01 13:52:37 +0000

    add animation effect, setup basic structure

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            console.log(entry)
+            if (entry.isIntersecting){
+                entry.target.classList.add('show');
+            } else{
+                entry.target.classList.remove('show');
+            }
+        });
+    });
+
+
+
+    var hiddenSec = document.querySelectorAll(".hidden");
+    hiddenSec.forEach((el) => observer.observe(el));
+
+});

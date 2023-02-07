e04ff7a1cc597db91872f36b409487506b7c2432
Seb <jonesseb10@gmail.com>
2023-02-07 15:15:31 +0000

    update project section with all buttons working

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
 // Project buttons
 var weatherButton = $("#weatherBtn");
 var quizButton = $("#quizBtn");
+var twinCitiesButton = $("#twinCitiesBtn");
 var passwordButton = $("#passGenBtn");
 
 // Project elements
@@ -50,9 +51,20 @@ weatherButton.on("click",function(){
 
     // Change header text 
     projectHeader.text("Weather Dashboard");
+    projectHeader.addClass("projectHeader");
 
     var weatherImg = $("<img src='./assets/images/weather_dashboard.png' class='projectImg' alt='Image of weather dashboard'>");
     imageContainer.append(weatherImg);
+
+    projectP.text("Tech Stack: jQuery, Bootstrap, HTML");
+
+    var projectDesc = $("<p>");
+    var projectDesc2 = $("<p>");
+    projectDesc.text("Weather Dashboard using OpenWeatherMap API and Javascript to create dynamic HTML");
+    projectDesc2.text("Using client storage to save previous searches");
+    imageContainer.append(projectDesc,projectDesc2);
+
+
 })
 
 // Code Quiz Project
@@ -60,14 +72,64 @@ quizButton.on("click",function(){
 
     clearContents();
     projectHeader.text("Coding Quiz");
+    projectHeader.addClass("projectHeader");
+
+    var weatherImg = $("<img src='./assets/images/code_quiz.png' class='projectImg' alt='Image of coding quiz'>");
+    imageContainer.append(weatherImg);
+
+    projectP.text("Tech Stack: jQuery, Bootstrap, HTML");
+
+    var projectDesc = $("<p>");
+    var projectDesc2 = $("<p>");
+    projectDesc.text("Coding quiz using functions and methods to quiz a user about topics");
+    projectDesc2.text("Using client storage to save user score");
+    imageContainer.append(projectDesc,projectDesc2);
+
 
 })
+
+// Twin Cities Project
+twinCitiesButton.on("click",function(){
+
+    // Clear any previous HTML
+    clearContents();
+
+    // Change header text 
+    projectHeader.text("Twin Cities Application (WIP)");
+    projectHeader.addClass("projectHeader");
+
+    var weatherImg = $("<img src='./assets/images/twinCityImage.png' class='projectImg' alt='Image of twin cities app'>");
+    imageContainer.append(weatherImg);
+
+    projectP.text("Tech Stack: PHP, Javascript, mySQL, Bootstrap, HTML");
+
+    var projectDesc = $("<p>");
+    var projectDesc2 = $("<p>");
+    projectDesc.text("Map & weather dashboard to display information about twin cities, Munich & Edinburgh");
+    projectDesc2.text("Using mySQL database to store cities data");
+    imageContainer.append(projectDesc,projectDesc2);
+
+
+})
+
 
 // Password Generator Project
 passwordButton.on("click",function(){
 
     clearContents();
     projectHeader.text("Password Generator");
+    projectHeader.addClass("projectHeader");
+
+    var weatherImg = $("<img src='./assets/images/pass-generator.png' class='projectImg' alt='Image of password generator application'>");
+    imageContainer.append(weatherImg);
+
+    projectP.text("Tech Stack: jQuery, HTML");
+
+    var projectDesc = $("<p>");
+    var projectDesc2 = $("<p>");
+    projectDesc.text("Using alerts to gather user input to create a secure randomly generated password");
+    imageContainer.append(projectDesc,projectDesc2);
+
 
 })
 

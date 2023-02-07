6e4eb71f120b5858158039bb8d873ac603c1b48b
Seb <jonesseb10@gmail.com>
2023-02-07 13:34:01 +0000

    update styling on project section

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
 
+
+
+// Carousel buttons
+var nextBtn = $("#nextBtn");
+var prevBtn = $("#prevBtn");
+
+
 // Add observer to listen for which elements are viewable
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
@@ -23,6 +30,19 @@ hidden1Sec.forEach((el) => observer.observe(el));
 $("#work-btn").on("click",function(){
 })
 
+nextBtn.on("click",function(event){
+    event.preventDefault();
+    console.log("Next button");
+
+    // Create function to change carousel to next slide in set  
+    // On click
+        // Change 
+})
+
+prevBtn.on("click",function(event){
+    event.preventDefault();
+    console.log("Previous button");
+})
 
 
 

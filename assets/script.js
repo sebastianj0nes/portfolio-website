a715226ef6792ddffb6b10e9defe7ac4e32b92a8
Seb <jonesseb10@gmail.com>
2023-02-07 11:20:53 +0000

    add aboutme jumbotron and add main image

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,29 @@
 $(document).ready(function(){
 
-
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-            console.log(entry)
-            if (entry.isIntersecting){
-                entry.target.classList.add('show');
-            } else{
-                entry.target.classList.remove('show');
-            }
-        });
+// Add observer to listen for which elements are viewable
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('show');
+        } else{
+            entry.target.classList.remove('show');
+        }
     });
+});
+
+// Target sections and add observers to section
+var hiddenSec = document.querySelectorAll(".hidden");
+hiddenSec.forEach((el) => observer.observe(el));
+var hidden1Sec = document.querySelectorAll(".hidden1");
+hidden1Sec.forEach((el) => observer.observe(el));
+
+
+// Add event listener for button click
+$("#work-btn").on("click",function(){
+})
 
 
 
-    var hiddenSec = document.querySelectorAll(".hidden");
-    hiddenSec.forEach((el) => observer.observe(el));
-    var hidden1Sec = document.querySelectorAll(".hidden1");
-    hidden1Sec.forEach((el) => observer.observe(el));
 
 });
